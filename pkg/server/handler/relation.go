4d660ccf5838476d7e26067f92dd4397a019c8ca
stevenchan <stevenchan@oursky.com>
2017-06-30 17:34:06 +0800

    Update implementation of user related handler
    Keep the API signature unchanged, while username and email would be
    stored in user Record separately.
    
    Some test cases use gomock database instead of database injection,
    because the method Query is hard to be implemented in mock.

--- pkg/server/handler/relation.go
+++ pkg/server/handler/relation.go
@@ -143,11 +143,17 @@ func (h *RelationQueryHandler) Handle(rpayload *router.Payload, response *router
 		})
 	resultList := make([]interface{}, 0, len(result))
 	for _, userinfo := range result {
+		data := map[string]interface{}{}
+		if err := makeUserData(rpayload.Database, rpayload.DBConn, userinfo.ID, &data); err != nil {
+			response.Err = skyerr.MakeError(err)
+			return
+		}
+
 		resultList = append(resultList, struct {
 			ID   string      `json:"id"`
 			Type string      `json:"type"`
 			Data interface{} `json:"data"`
-		}{userinfo.ID, "user", userinfo})
+		}{userinfo.ID, "user", data})
 	}
 	response.Result = resultList
 	count, countErr := rpayload.DBConn.QueryRelationCount(
@@ -271,14 +277,17 @@ func (h *RelationAddHandler) Handle(rpayload *router.Payload, response *router.R
 				Data skyerr.Error `json:"data"`
 			}{target, "error", skyerr.NewResourceFetchFailureErr("user", target)})
 		} else {
-			userinfo := skydb.AuthInfo{}
-			rpayload.DBConn.GetUser(target, &userinfo)
-			userinfo.HashedPassword = []byte{}
+			data := map[string]interface{}{}
+			if err := makeUserData(rpayload.Database, rpayload.DBConn, target, &data); err != nil {
+				response.Err = skyerr.MakeError(err)
+				return
+			}
+
 			results = append(results, struct {
 				ID   string      `json:"id"`
 				Type string      `json:"type"`
 				Data interface{} `json:"data"`
-			}{target, "user", userinfo})
+			}{target, "user", data})
 		}
 	}
 	response.Result = results
@@ -350,4 +359,27 @@ func (h *RelationRemoveHandler) Handle(rpayload *router.Payload, response *route
 		}
 	}
 	response.Result = results
+}
+
+func makeUserData(db skydb.Database, conn skydb.Conn, userID string, data *map[string]interface{}) error {
+	userinfo := skydb.AuthInfo{}
+	conn.GetAuth(userID, &userinfo)
+
+	// TODO: return user record
+	user := skydb.Record{}
+	if err := db.Get(skydb.NewRecordID("user", userID), &user); err != nil {
+		// TODO: handle auth without user in a better way
+		return err
+	}
+
+	(*data)["_id"] = userinfo.ID
+	if username, _ := user.Data["username"].(string); username != "" {
+		(*data)["username"] = username
+	}
+
+	if email, _ := user.Data["email"].(string); email != "" {
+		(*data)["email"] = email
+	}
+
+	return nil
 }
